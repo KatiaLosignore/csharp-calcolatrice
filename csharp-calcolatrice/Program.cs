fce236d291d0d9c33c542ce64c2546bfd55f6684
KatiaLosignore <katialos.kl@gmail.com>
2023-09-12 15:32:47 +0200

    added Program with double number

--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -23,3 +23,24 @@ Console.WriteLine($"The smaller of these two numbers {b} e {c} is: {CalcoliHelpe
 
 Console.WriteLine($"The greater of these two numbers {c} e {d} is: {CalcoliHelper.MaxNumber(c, d)};");
 
+
+// Dichiaro i miei numeri double
+
+double e = -1.2;
+double f = 3.5;
+double g = -2.5;
+double h = 6.5;
+
+Console.WriteLine("----------------------------------------------------");
+
+Console.WriteLine($"The sum of these two numbers {f} e {h} is: {CalcoliHelper.SumNumber(f, h)};");
+
+Console.WriteLine($"The subtraction of these two numbers {h} e {f} is: {CalcoliHelper.SubNumber(h, f)};");
+
+Console.WriteLine($"The multiplication of these two numbers {f} e {h} is: {CalcoliHelper.MultiNumber(f, h)};");
+
+Console.WriteLine($"The absolute value of {e} is : {CalcoliHelper.AbsoluteNumber(e)};");
+
+Console.WriteLine($"The smaller of these two numbers {f} e {h} is: {CalcoliHelper.MinNumber(f, h)};");
+
+Console.WriteLine($"The greater of these two numbers {f} e {h} is: {CalcoliHelper.MaxNumber(f, h)};");
